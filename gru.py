cc17c02c2eff6824c0694fb53acbc81eb69ceb88
AnujJha88 <anujjha8788@gmail.com>
2025-08-04 11:46:55 +0530

    gru zeroing gradients

--- gru.py
+++ gru.py
@@ -1,5 +1,12 @@
 class GRU:
-    def __init__():
+    def __init__(self):
         self.forget_gate=ForgetGate()
         self.update_gate=UpdateGate()
+        self.input=None
+        self.output=None
+        self.zero_grad()
+
+    def zero_grad(self):
+        self.forget_gate.zero_grad()
+        self.update_gate.zero_grad()
 

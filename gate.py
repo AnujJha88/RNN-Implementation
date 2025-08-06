ce558c36d67abded1bc8ba1ca6735eba6c0653dd
AnujJha88 <anujjha8788@gmail.com>
2025-08-07 03:46:55 +0530

    fixed zero grad and designed forward for gru

--- gate.py
+++ gate.py
@@ -1,11 +1,11 @@
 import numpy as np
 from activation import Sigmoid
 
-def zero_grad(W,U,b,dW,dU,db):
-    dW = np.zeros_like(W)  
-    dU = np.zeros_like(U)  
-    db = np.zeros_like(b) 
-    cache = None 
+def _zero_grad(W,U,b,dW,dU,db):
+    dW.fill(0)
+    dU.fill(0)
+    db.fill(0)
+    cache = None
     return W,U,b,cache
 
 class MulGate:
@@ -67,22 +67,22 @@ class AddGate:
         grad_y = grad_output
         return grad_x, grad_y
 
-class ForgetGate:  # LSTM   
+class ForgetGate:  # LSTM
     def __init__(self):
-        self.Wf = None  
-        self.Uf = None  
-        self.bf = None 
-        self.cache = None 
-    
+        self.Wf = None
+        self.Uf = None
+        self.bf = None
+        self.cache = None
+
     def forward(self, x, h_prev, c_prev):
         """
         Forward pass for forget gate
-        
+
         Args:
             x: Input at current timestep (batch_size, input_dim)
             h_prev: Hidden state from previous timestep (batch_size, hidden_dim)
             c_prev: Cell state from previous timestep (batch_size, hidden_dim)
-            
+
         Returns:
             f: Forget gate activation (batch_size, hidden_dim)
         """
@@ -90,22 +90,22 @@ class ForgetGate:  # LSTM
         linear = np.dot(x, self.Wf.T) + np.dot(h_prev, self.Uf.T) + self.bf
         # Sigmoid activation
         f = Sigmoid.forward(linear)
-        
+
         # Cache values for backward pass
         self.cache = (x, h_prev, f)
-        
+
         return f
-    
+
     def backward(self, df, dnext_h, dnext_c, dnext_f):
         """
         Backward pass for forget gate
-        
+
         Args:
             df: Gradient of loss w.r.t forget gate output (batch_size, hidden_dim)
             dnext_h: Gradient from next hidden state (batch_size, hidden_dim)
             dnext_c: Gradient from cell state (batch_size, hidden_dim)
             dnext_f: Gradient from next forget gate (batch_size, hidden_dim)
-            
+
         Returns:
             dx: Gradient w.r.t input (batch_size, input_dim)
             dh_prev: Gradient w.r.t previous hidden state (batch_size, hidden_dim)
@@ -114,39 +114,39 @@ class ForgetGate:  # LSTM
             dbf: Gradient w.r.t bias (hidden_dim,)
         """
         x, h_prev, f = self.cache
-        
+
         # Compute gradient through sigmoid
         dsigma = Sigmoid.backward(f) * (df + dnext_h + dnext_c)
-        
+
         # Compute gradients for parameters
         dWf = np.dot(dsigma.T, np.hstack([x, h_prev]))
         dUf = np.dot(dsigma.T, h_prev)
         dbf = np.sum(dsigma, axis=0)
-        
+
         # Compute gradients for input and previous hidden state
         dx = np.dot(dsigma, self.Wf[:, :x.shape[1]])
         dh_prev = np.dot(dsigma, self.Wf[:, x.shape[1]:])
-        
+
         return dx, dh_prev, dWf, dUf, dbf
 
     def zero_grad(self):
-        zero_grad(self.Wf,self.Uf,self.bf,self.dWf,self.dUf,self.dbf)
+        _zero_grad(self.Wf,self.Uf,self.bf,self.dWf,self.dUf,self.dbf)
 class InputGate:  # LSTM
     def __init__(self):
-        self.Wi = None  
-        self.Ui = None  
-        self.bi = None  
-        self.cache = None  
-    
+        self.Wi = None
+        self.Ui = None
+        self.bi = None
+        self.cache = None
+
     def forward(self, x, h_prev, c_prev):
         """
         Forward pass for input gate
-        
+
         Args:
             x: Input at current timestep (batch_size, input_dim)
             h_prev: Hidden state from previous timestep (batch_size, hidden_dim)
             c_prev: Cell state from previous timestep (batch_size, hidden_dim)
-            
+
         Returns:
             i: Input gate activation (batch_size, hidden_dim)
         """
@@ -154,22 +154,22 @@ class InputGate:  # LSTM
         linear = np.dot(x, self.Wi.T) + np.dot(h_prev, self.Ui.T) + self.bi
         # Sigmoid activation
         i = Sigmoid.forward(linear)
-        
+
         # Cache values for backward pass
         self.cache = (x, h_prev, i)
-        
+
         return i
-    
+
     def backward(self, di, dnext_h, dnext_c, dnext_i):
         """
         Backward pass for input gate
-        
+
         Args:
             di: Gradient of loss w.r.t input gate output (batch_size, hidden_dim)
             dnext_h: Gradient from next hidden state (batch_size, hidden_dim)
             dnext_c: Gradient from cell state (batch_size, hidden_dim)
             dnext_i: Gradient from next input gate (batch_size, hidden_dim)
-            
+
         Returns:
             dx: Gradient w.r.t input (batch_size, input_dim)
             dh_prev: Gradient w.r.t previous hidden state (batch_size, hidden_dim)
@@ -178,39 +178,39 @@ class InputGate:  # LSTM
             dbi: Gradient w.r.t bias (hidden_dim,)
         """
         x, h_prev, i = self.cache
-        
+
         # Compute gradient through sigmoid
         dsigma = Sigmoid.backward(i) * (di + dnext_h + dnext_i)
-        
+
         # Compute gradients for parameters
         dWi = np.dot(dsigma.T, x)
         dUi = np.dot(dsigma.T, h_prev)
         dbi = np.sum(dsigma, axis=0)
-        
+
         # Compute gradients for input and previous hidden state
         dx = np.dot(dsigma, self.Wi)
         dh_prev = np.dot(dsigma, self.Ui)
-        
+
         return dx, dh_prev, dWi, dUi, dbi
     def zero_grad(self):
-        zero_grad(self.Wi,self.Ui,self.bi,self.dWi,self.dUi,self.dbi)
+        _zero_grad(self.Wi,self.Ui,self.bi,self.dWi,self.dUi,self.dbi)
 
 class OutputGate:  # LSTM
     def __init__(self):
-        self.Wo = None  
-        self.Uo = None  
-        self.bo = None  
-        self.cache = None  
-    
+        self.Wo = None
+        self.Uo = None
+        self.bo = None
+        self.cache = None
+
     def forward(self, x, h_prev, c_prev):
         """
         Forward pass for output gate
-        
+
         Args:
             x: Input at current timestep (batch_size, input_dim)
             h_prev: Hidden state from previous timestep (batch_size, hidden_dim)
             c_prev: Cell state from previous timestep (batch_size, hidden_dim)
-            
+
         Returns:
             o: Output gate activation (batch_size, hidden_dim)
         """
@@ -218,22 +218,22 @@ class OutputGate:  # LSTM
         linear = np.dot(x, self.Wo.T) + np.dot(h_prev, self.Uo.T) + self.bo
         # Sigmoid activation
         o = Sigmoid.forward(linear)
-        
+
         # Cache values for backward pass
         self.cache = (x, h_prev, o)
-        
+
         return o
-    
+
     def backward(self, do, dnext_h, dnext_c, dnext_o):
         """
         Backward pass for output gate
-        
+
         Args:
             do: Gradient of loss w.r.t output gate (batch_size, hidden_dim)
             dnext_h: Gradient from next hidden state (batch_size, hidden_dim)
             dnext_c: Gradient from cell state (batch_size, hidden_dim)
             dnext_o: Gradient from next output gate (batch_size, hidden_dim)
-            
+
         Returns:
             dx: Gradient w.r.t input (batch_size, input_dim)
             dh_prev: Gradient w.r.t previous hidden state (batch_size, hidden_dim)
@@ -242,62 +242,62 @@ class OutputGate:  # LSTM
             dbo: Gradient w.r.t bias (hidden_dim,)
         """
         x, h_prev, o = self.cache
-        
+
         # Compute gradient through sigmoid
         dsigma = Sigmoid.backward(o) * (do + dnext_h + dnext_o)
-        
+
         # Compute gradients for parameters
         dWo = np.dot(dsigma.T, x)
         dUo = np.dot(dsigma.T, h_prev)
         dbo = np.sum(dsigma, axis=0)
-        
+
         # Compute gradients for input and previous hidden state
         dx = np.dot(dsigma, self.Wo)
         dh_prev = np.dot(dsigma, self.Uo)
-        
+
         return dx, dh_prev, dWo, dUo, dbo
     def zero_grad(self):
-        zero_grad(self.Wo,self.Uo,self.bo,self.dWo,self.dUo,self.dbo)
+        _zero_grad(self.Wo,self.Uo,self.bo,self.dWo,self.dUo,self.dbo)
 
 class CandidateGate:  # LSTM
     def __init__(self):
-        self.Wc = None  
-        self.Uc = None  
-        self.bc = None  
-        self.cache = None  
-    
+        self.Wc = None
+        self.Uc = None
+        self.bc = None
+        self.cache = None
+
     def forward(self, x, h_prev):
         """
         Forward pass for candidate gate (cell state update)
-        
+
         Args:
             x: Input at current timestep (batch_size, input_dim)
             h_prev: Hidden state from previous timestep (batch_size, hidden_dim)
-            
+
         Returns:
             c_tilde: Candidate cell state (batch_size, hidden_dim)
         """
         from activation import Tanh  # Import here to avoid circular import
-        
+
         # Linear transformation
         linear = np.dot(x, self.Wc.T) + np.dot(h_prev, self.Uc.T) + self.bc
         # Tanh activation
         c_tilde = Tanh.forward(linear)
-        
+
         # Cache values for backward pass
         self.cache = (x, h_prev, c_tilde)
-        
+
         return c_tilde
-    
+
     def backward(self, dc_tilde, dnext_h, dnext_c):
         """
         Backward pass for candidate gate
-        
+
         Args:
             dc_tilde: Gradient of loss w.r.t candidate cell state (batch_size, hidden_dim)
             dnext_h: Gradient from next hidden state (batch_size, hidden_dim)
             dnext_c: Gradient from next cell state (batch_size, hidden_dim)
-            
+
         Returns:
             dx: Gradient w.r.t input (batch_size, input_dim)
             dh_prev: Gradient w.r.t previous hidden state (batch_size, hidden_dim)
@@ -306,40 +306,40 @@ class CandidateGate:  # LSTM
             dbc: Gradient w.r.t bias (hidden_dim,)
         """
         from activation import Tanh  # Import here to avoid circular import
-        
+
         x, h_prev, c_tilde = self.cache
-        
+
         # Compute gradient through tanh
         dtanh = Tanh.backward(c_tilde) * (dc_tilde + dnext_h + dnext_c)
-        
+
         # Compute gradients for parameters
         dWc = np.dot(dtanh.T, x)
         dUc = np.dot(dtanh.T, h_prev)
         dbc = np.sum(dtanh, axis=0)
-        
+
         # Compute gradients for input and previous hidden state
         dx = np.dot(dtanh, self.Wc)
         dh_prev = np.dot(dtanh, self.Uc)
-        
+
         return dx, dh_prev, dWc, dUc, dbc
     def zero_grad(self):
-        zero_grad(self.Wc,self.Uc,self.bc,self.dWc,self.dUc,self.dbc)
+        _zero_grad(self.Wc,self.Uc,self.bc,self.dWc,self.dUc,self.dbc)
 
 class UpdateGate:  # GRU
     def __init__(self):
-        self.Wz = None  
-        self.Uz = None  
-        self.bz = None  
-        self.cache = None  
-    
+        self.Wz = None
+        self.Uz = None
+        self.bz = None
+        self.cache = None
+
     def forward(self, x, h_prev):
         """
         Forward pass for update gate
-        
+
         Args:
             x: Input at current timestep (batch_size, input_dim)
             h_prev: Hidden state from previous timestep (batch_size, hidden_dim)
-            
+
         Returns:
             z: Update gate activation (batch_size, hidden_dim)
         """
@@ -347,21 +347,21 @@ class UpdateGate:  # GRU
         linear = np.dot(x, self.Wz.T) + np.dot(h_prev, self.Uz.T) + self.bz
         # Sigmoid activation
         z = Sigmoid.forward(linear)
-        
+
         # Cache values for backward pass
         self.cache = (x, h_prev, z)
-        
+
         return z
-    
+
     def backward(self, dz, dnext_h, dnext_z):
         """
         Backward pass for update gate
-        
+
         Args:
             dz: Gradient of loss w.r.t update gate (batch_size, hidden_dim)
             dnext_h: Gradient from next hidden state (batch_size, hidden_dim)
             dnext_z: Gradient from next update gate (batch_size, hidden_dim)
-            
+
         Returns:
             dx: Gradient w.r.t input (batch_size, input_dim)
             dh_prev: Gradient w.r.t previous hidden state (batch_size, hidden_dim)
@@ -370,37 +370,37 @@ class UpdateGate:  # GRU
             dbz: Gradient w.r.t bias (hidden_dim,)
         """
         x, h_prev, z = self.cache
-        
+
         # Compute gradient through sigmoid
         dsigma = Sigmoid.backward(z) * (dz + dnext_h + dnext_z)
-        
+
         # Compute gradients for parameters
         dWz = np.dot(dsigma.T, x)
         dUz = np.dot(dsigma.T, h_prev)
         dbz = np.sum(dsigma, axis=0)
-        
+
         # Compute gradients for input and previous hidden state
         dx = np.dot(dsigma, self.Wz)
         dh_prev = np.dot(dsigma, self.Uz)
-        
+
         return dx, dh_prev, dWz, dUz, dbz
     def zero_grad(self):
-        zero_grad(self.Wz,self.Uz,self.bz,self.dWz,self.dUz,self.dbz)
+        _zero_grad(self.Wz,self.Uz,self.bz,self.dWz,self.dUz,self.dbz)
 class ResetGate:  # GRU
     def __init__(self):
-        self.Wr = None  
-        self.Ur = None  
-        self.br = None  
-        self.cache = None  
-    
+        self.Wr = None
+        self.Ur = None
+        self.br = None
+        self.cache = None
+
     def forward(self, x, h_prev):
         """
         Forward pass for reset gate
-        
+
         Args:
             x: Input at current timestep (batch_size, input_dim)
             h_prev: Hidden state from previous timestep (batch_size, hidden_dim)
-            
+
         Returns:
             r: Reset gate activation (batch_size, hidden_dim)
         """
@@ -408,21 +408,21 @@ class ResetGate:  # GRU
         linear = np.dot(x, self.Wr.T) + np.dot(h_prev, self.Ur.T) + self.br
         # Sigmoid activation
         r = Sigmoid.forward(linear)
-        
+
         # Cache values for backward pass
         self.cache = (x, h_prev, r)
-        
+
         return r
-    
+
     def backward(self, dr, dnext_h, dnext_r):
         """
         Backward pass for reset gate
-        
+
         Args:
             dr: Gradient of loss w.r.t reset gate (batch_size, hidden_dim)
             dnext_h: Gradient from next hidden state (batch_size, hidden_dim)
             dnext_r: Gradient from next reset gate (batch_size, hidden_dim)
-            
+
         Returns:
             dx: Gradient w.r.t input (batch_size, input_dim)
             dh_prev: Gradient w.r.t previous hidden state (batch_size, hidden_dim)
@@ -431,19 +431,19 @@ class ResetGate:  # GRU
             dbr: Gradient w.r.t bias (hidden_dim,)
         """
         x, h_prev, r = self.cache
-        
+
         # Compute gradient through sigmoid
         dsigma = Sigmoid.backward(r) * (dr + dnext_h + dnext_r)
-        
+
         # Compute gradients for parameters
         dWr = np.dot(dsigma.T, x)
         dUr = np.dot(dsigma.T, h_prev)
         dbr = np.sum(dsigma, axis=0)
-        
+
         # Compute gradients for input and previous hidden state
         dx = np.dot(dsigma, self.Wr)
         dh_prev = np.dot(dsigma, self.Ur)
-        
+
         return dx, dh_prev, dWr, dUr, dbr
     def zero_grad(self):
-        zero_grad(self.Wr,self.Ur,self.br,self.dWr,self.dUr,self.dbr)
+        _zero_grad(self.Wr,self.Ur,self.br,self.dWr,self.dUr,self.dbr)
--- gru.py
+++ gru.py
@@ -1,19 +1,33 @@
+import numpy as np
+from gate import UpdateGate,ResetGate,CandidateGate
+
 class GRU:
-    def __init__(self):
-        self.forget_gate=ForgetGate()
+    def __init__(self, input_size, hidden_size, output_size):
+        self.hidden_size = hidden_size
+        self.input_size = input_size
+        self.output_size = output_size
+
         self.update_gate=UpdateGate()
-        self.input=None
-        self.output=None
-        self.zero_grad()
+        self.reset_gate=ResetGate()
+        self.candidate_gate=CandidateGate()
 
-    def zero_grad(self):
-        self.forget_gate.zero_grad()
-        self.update_gate.zero_grad()
-        self.input=None
-        self.output=None
+    def forward(self,input_seq,h_0):
+        seq_len=input_seq.shape[0]
 
-    def forward(self,x,h_prev,c_prev):
-        pass
+        h_dim=self.hidden_size
+        h_seq=np.zeros((seq_len,h_dim))
+        if h_0 is None:
+            h_prev=np.zeroes(hidden_size)
+        else:
+            h_prev=h_0
 
-    def backward(self,dnext_h,dnext_c):
-        pass
+        for t in range(seq_len):
+            x_t=input_seq[t]
+            z_t=self.update_gate.forward(x_t,h_prev)
+            r_t=self.reset_gate.forward(x_t,h_prev)
+
+            h_tilde=self.candidate_gate.forward(x_t,r_t*h_prev)
+            h_t=(1-z_t)*h_prev+z_t*h_tilde
+            h_seq[t]=h_t
+            h_prev=h_t
+        return h_seq
